f6dce29b0ccba41ef3807243b6428c8bb0b9c986
Kenneth Gorking <kgorking@outlook.com>
2025-03-27 14:37:38 +0100

    Fix path to excel file sometimes changing

--- PDFDownloader/tests/test_excel_reader.py
+++ PDFDownloader/tests/test_excel_reader.py
@@ -1,3 +1,4 @@
+import os
 from utils.xlsx_chunk_reader import read_xlsx_in_chunks
 
 def test_excel_reader():
@@ -7,10 +8,12 @@ def test_excel_reader():
     Chunking sometimes goes wrong.
     """
 
+    excel_file = os.path.dirname(os.path.abspath(__file__)) + '/../data/GRI_2017_2020 (1).xlsx'
+
     # The 'read_xlsx_in_chunks' functions opens the excel file on
     # every iteration, which takes about 2 seconds.
     # To just check the row-count, this test needs about 45 seconds to run.
-    chunk_generator = read_xlsx_in_chunks('data/GRI_2017_2020 (1).xlsx', chunk_size=1000)
+    chunk_generator = read_xlsx_in_chunks(excel_file, chunk_size=1000)
 
     count = 0
     for chunk in chunk_generator:
